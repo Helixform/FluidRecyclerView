f4d6ca9b1ef2fc3860eddf72d59930f81ea83ff4
ktiays <cuansky@gmail.com>
2023-09-08 21:36:41 +0800

    Replace spring back calculation unit with milliseconds

--- fluidrecyclerview/src/main/java/org/helixform/fluidrecyclerview/FluidSpringBack.kt
+++ fluidrecyclerview/src/main/java/org/helixform/fluidrecyclerview/FluidSpringBack.kt
@@ -17,17 +17,22 @@ class FluidSpringBack {
         val isFinished: Boolean
     )
 
-    fun absorb(velocity: Float, distance: Float) {
+    fun absorb(velocity: Float, initialValue: Float) {
         lambda = 2.0f * PI.toFloat() / 0.575f
-        c1 = distance
-        c2 = velocity + lambda * distance
+        c1 = initialValue
+        // The formula needs to be calculated in units of points per second.
+        c2 = velocity * 1000f + lambda * initialValue
     }
 
     fun value(time: Float): SpringBackValue {
-        val offset = (c1 + c2 * time) * exp(-lambda * time)
+        // Convert time from milliseconds to seconds.
+        val t = time / 1000f
+        val offset = (c1 + c2 * t) * exp(-lambda * t)
 
-        val velocity = velocityAt(time)
-        return if (abs(offset) < valueThreshold && abs(velocity) < velocityThreshold) {
+        val velocity = velocityAt(t)
+        // The velocity threshold is in units of points per millisecond.
+        // We need to convert velocity to match the unit.
+        return if (abs(offset) < valueThreshold && abs(velocity) / 1000f < velocityThreshold) {
             SpringBackValue(0f, true)
         } else {
             SpringBackValue(offset, false)
